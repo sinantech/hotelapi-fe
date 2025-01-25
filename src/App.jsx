b4119ba7961f5dced719b83538903cc8ab06a7c5
sinantech <sinancemozer@gmail.com>
2025-01-26 00:44:09 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import AddRoom from "./components/room/AddRoom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <AddRoom />
+    </>
+  );
+}
+
+export default App;
